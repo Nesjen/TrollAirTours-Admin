0d1469f69959765423aa4cdc211962727dacac75
Eirik Nesje <ebnesjen@gmail.com>
2015-10-25 20:49:35 +0100

    div

--- admin/view/home.php
+++ admin/view/home.php
@@ -2,7 +2,7 @@
         <div class="row" style="padding-top: 10%;">
             <div class="col-md-4">
                 <a href="?page=aircraft">
-                    <img class="center-block" src="image/flighticon.png "> 
+                    <img class="center-block" src="image/aircraftmanager.png "> 
                     <h3 class="row text-center">Aircraft Manager</h3>
                 </a>
             </div>
@@ -14,7 +14,7 @@
             </div>
             <div class="col-md-4">
                 <a href="?page=employee">
-                    <img class="center-block" src="image/usericon.png">
+                    <img class="center-block" src="image/employee.png">
                     <h3 class="row text-center">Employee Manager</h3>
                 </a>
             </div>
@@ -25,7 +25,7 @@
         <div class="row" style="padding-top: 5%;">
             <div class="col-md-4">
                 <a href="?page=customer">
-                    <img class="center-block" src="image/usericon.png">
+                    <img class="center-block" src="image/user.png">
                     <h3 class="row text-center">Customer Manager</h3>
                 </a>
             </div>
@@ -61,7 +61,7 @@
             </div>
             <div class="col-md-4">
                 <a href="?page=adminUser">
-                    <img class="center-block" src="image/usericon.png">
+                    <img class="center-block" src="image/adminmanager.png">
                     <h3 class="row text-center">AdminUser Manager</h3>
                 </a>
             </div>
