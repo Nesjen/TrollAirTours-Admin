69511db330a543270b1bb61f44d5a2f8d1d5d3b8
Blaalid <Blaalid@me.com>
2015-12-05 11:38:19 +0100

    noko kommentarar

--- admin/controller/BookingController.php
+++ admin/controller/BookingController.php
@@ -1,45 +1,54 @@
 <?php
 /**
  * BookingController - Troll Air Tours Admin Interface.
- * 
- *  
  */
 
 require_once("Controller.php");
 
 class BookingController extends Controller {
 
-
+/*
+ * If the current page is 'customBooking' it will run the showCustomBookingAction(),
+ * else if the page is 'booking' it will run showBookingAction().
+ */
     public function show($page) {
-        if ($page == "customBooking") {
-            $this->ShowCustomBookingAction();
-        } else if ($page == "booking") {
-            $this->showBookingAction();
+        if ($page == "customBooking") { //Checks if page is set to 'customBooking'
+            $this->ShowCustomBookingAction(); //Runs showCustomBookingAction()
+        } else if ($page == "booking") { //Checks if page is set to 'booking'
+            $this->showBookingAction(); //Runs showBookingAction()
         } 
     }
     
-
+/*
+ * Receives the booking-model, then gets all custom bookings and adds them to an array then
+ * renders next site.
+ * #RENDER customBooking
+ */
     private function ShowCustomBookingAction() {
-        $bookingModel = $GLOBALS["bookingModel"];
-        $customBookings = $bookingModel->getAllCustom();
+        $bookingModel = $GLOBALS["bookingModel"]; //Gets booking-model
+        $customBookings = $bookingModel->getAllCustom(); //Fetches all custom bookings
 
-        $data = array(
+        $data = array( //Puts custom booking in a array variable
             "customBookings" => $customBookings,     
         );
         
-        return $this->render("customBooking", $data);
+        return $this->render("customBooking", $data); //Takes view part(customBooking) and model part (data) and renders the page.
     }
     
-    
+/*
+ * Receives the booking-model, then gets all bookings that are NOT custom and adds them to an array then
+ * renders next site.
+ * #RENDER booking
+ */
     private function showBookingAction() {
-        $bookingModel = $GLOBALS["bookingModel"];
-        $bookings = $bookingModel->getAllPre();
+        $bookingModel = $GLOBALS["bookingModel"]; //Gets booking-model
+        $bookings = $bookingModel->getAllPre(); //Fetches all NON custom bookings
 
-        $data = array(
+        $data = array( //Puts bookings in a array variable
             "bookings" => $bookings,     
         );
         
-        return $this->render("booking", $data);
+        return $this->render("booking", $data); //Takes view part(booking) and model part (data) and renders the page.
     }
    
     
